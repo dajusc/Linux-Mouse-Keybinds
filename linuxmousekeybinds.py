1de09ef58a1926f06ed0a7d07e2d584d896d21c7
David Schaefer <github@schaeferdavid.de>
2018-09-24 01:15:02 +0200

    support key-press-and-hold (extensive refactoring) Drawback: Sequential keystrokes are (currently) no longer supported.
    Signed-off-by: David Schaefer <github@schaeferdavid.de>

--- linuxmousekeybinds.py
+++ linuxmousekeybinds.py
@@ -31,12 +31,6 @@ class linuxmousekeybinds():
         self.running = False  # Is the daemon running? (managed internally).
         self.do_stop = True  # Shall the daemon stop running? (managed internally).
         self.bindbypid = False  # False: Do not check for PID, just windowname (managed internally).
-        self.evls = {"btn-down": 1,
-                     "btn-up": 0,
-                     "whl-down": -1,
-                     "whl-up": 1,
-                     "whl-right": -1,
-                     "whl-left": 1}  # Event-value dictionary ().
         #--
         self.devs = {}  # database of all available devices
         self.cfgs = {}  # database of configs
@@ -78,17 +72,14 @@ class linuxmousekeybinds():
             if type(names) != list:
                 names = [names]
             for name in names:
-                if name.startswith("BTN_") or name.startswith("REL_"):
-                    name = name.upper()
+                name = name.upper()
+                if name.startswith("BTN_"):
                     self.btns[name] = int(code)
+                if name.startswith("REL_"):
+                    self.btns[name + "+"] = +int(code)
+                    self.btns[name + "-"] = -int(code)
 
-    def _to_int(self, string):
-        try:
-            return int(string)
-        except:
-            return None
-
-    def bind_key_to_button(self, appnam, btnnam, btndir, keynam, devnam=None):
+    def bind_key_to_button(self, appnam, btnnam, keynam, devnam=None):
         if type(appnam) == int:
             appnam = str(appnam)
             self.bindbypid = True
@@ -99,11 +90,6 @@ class linuxmousekeybinds():
             if self.verbose:
                 print("ERROR: Invalid button name. Must be one of: {}".format(self.btns.keys()))
             return
-        evvalu = self.evls.get(btndir, None)
-        if evvalu == None:
-            if self.verbose:
-                print("ERROR: Invalid button direction. Must be one of: {}".format(self.evls.keys()))
-            return
         #--
         if not devnam in self.cfgs:
             self.cfgs[devnam] = {}
@@ -111,30 +97,53 @@ class linuxmousekeybinds():
             self.cfgs[devnam][appnam] = {}
         if not evcode in self.cfgs[devnam][appnam]:
             self.cfgs[devnam][appnam][evcode] = {}
-        if not evvalu in self.cfgs[devnam][appnam][evcode]:
-            self.cfgs[devnam][appnam][evcode][evvalu] = {}
         #--
-        self.cfgs[devnam][appnam][evcode][evvalu] = keynam
+        self.cfgs[devnam][appnam][evcode] = keynam
 
-    def _get_keynam(self, appnam, evcode, evvalu, devnam=None):
+    def _get_keynam(self, appnam, evcode, devnam=None):
         if type(appnam) == int:
             appnam = str(appnam)
         if devnam == None:
             devnam = self.actdevnam
         if appnam not in self.cfgs.get(devnam, {}):
             appnam = None  # Default binding settings
-        return self.cfgs.get(devnam, {}).get(appnam, {}).get(evcode, {}).get(evvalu, None)
+        return self.cfgs.get(devnam, {}).get(appnam, {}).get(evcode, None)
 
-    def _do_keystrokes(self, appind, keynams):
-        # print round(time.time()), keynam
-        keynams = keynams.split(",")
-        for ind, keynam in enumerate(keynams):
-            cmd = "nice -n {} xdotool {} --window {} {}".format(self.nice, "{}", appind, keynam)
+    def _do_key(self, appind, keynam, down=True, up=True):
+        cmd = "nice -n {} xdotool {} --window {} {}".format(self.nice, "{}", appind, keynam)
+        if down:
             subprocess.Popen(cmd.format("keydown"), stdout=subprocess.PIPE, shell=True)
+        if down and up:
             time.sleep(self.delay)
+        if up:
             subprocess.Popen(cmd.format("keyup"), stdout=subprocess.PIPE, shell=True)
-            if ind != len(keynams):
-                time.sleep(self.delay)
+
+    def _to_int(self, string):
+        try:
+            return int(string)
+        except:
+            return None
+
+    def _get_active_window_index(self):
+        h = subprocess.Popen("xdotool getactivewindow", stdout=subprocess.PIPE, shell=True)
+        h.wait()
+        appind = h.stdout.read().decode('utf-8').strip()
+        appind = self._to_int(appind)
+        #--
+        return appind
+
+    def _get_window_name_and_pid(self, appind):
+        h = subprocess.Popen("xdotool getwindowname {}".format(appind), stdout=subprocess.PIPE, shell=True)
+        h.wait()
+        appnam = h.stdout.read().decode('utf-8').strip()
+        appnam = appnam.encode('ascii', 'ignore').decode('utf-8')
+        #--
+        h = subprocess.Popen("xdotool getwindowpid  {}".format(appind), stdout=subprocess.PIPE, shell=True)
+        h.wait()
+        apppid = h.stdout.read().decode('utf-8').strip()
+        apppid = self._to_int(apppid)
+        #--
+        return (appnam, apppid)
 
     def run(self, in_new_thread=True):
         if in_new_thread:
@@ -148,6 +157,8 @@ class linuxmousekeybinds():
         if self.verbose:
             print("Linux Mouse Keybinds started!")
         #--
+        EV_KEY = evdev.ecodes.EV_KEY
+        EV_REL = evdev.ecodes.EV_REL
         dev = self.devs[self.actdevnam]
         appind_last = None
         self.do_stop = False
@@ -156,28 +167,30 @@ class linuxmousekeybinds():
             while self.do_stop == False:
                 r, w, x = select.select([dev], [], [])
                 for event in dev.read():
-                    if event.type == evdev.ecodes.EV_KEY or (event.type == evdev.ecodes.EV_REL and event.code not in [0, 1]) or appind_last == None:
+                    evtype = event.type
+                    evcode = event.code
+                    evvalu = event.value
 
-                        h = subprocess.Popen("xdotool getactivewindow", stdout=subprocess.PIPE, shell=True)
-                        appind = h.stdout.read().decode('utf-8').strip()
-                        appind = self._to_int(appind)
+                    if (evtype not in [EV_KEY, EV_REL]) or (evtype == EV_REL and evcode in [0, 1]):
+                        continue
+                    if (evtype == EV_REL):
+                        evcode *= evvalu
 
-                        if appind not in [None, appind_last]:
-                            appind_last = appind
-                            h = subprocess.Popen("xdotool getwindowpid  {}".format(appind), stdout=subprocess.PIPE, shell=True)
-                            apppid = h.stdout.read().decode('utf-8').strip()
-                            apppid = self._to_int(apppid)
-                            h = subprocess.Popen("xdotool getwindowname {}".format(appind), stdout=subprocess.PIPE, shell=True)
-                            appnam = h.stdout.read().decode('utf-8').strip()
-                            if self.verbose and appnam != "":
-                                appnam = appnam.encode('ascii', 'ignore').decode('utf-8')
-                                print("Active window changed to \"{}\" (PID: {})".format(appnam, apppid))
+                    appind = self._get_active_window_index()
+                    if appind not in [None, appind_last]:
+                        appnam, apppid = self._get_window_name_and_pid(appind)
+                        if self.verbose and appnam != "":
+                            print("Active window changed to \"{}\" (PID: {})".format(appnam, apppid))
+                    appind_last = appind
 
-                        keynams = self._get_keynam(appnam, event.code, event.value) # binding based on windowname
-                        if keynams == None and self.bindbypid:
-                            keynams = self._get_keynam(apppid, event.code, event.value) # binding based on PID
-                        if keynams != None:
-                            self._do_keystrokes(appind, keynams)
+                    keynam = self._get_keynam(appnam, evcode) # binding based on windowname
+                    if keynam == None and self.bindbypid:
+                        keynam = self._get_keynam(apppid, evcode) # binding based on PID
+
+                    if keynam != None:
+                        down = (evtype == EV_KEY and evvalu == 1) or (evtype == EV_REL)
+                        up   = (evtype == EV_KEY and evvalu == 0) or (evtype == EV_REL)
+                        self._do_key(appind, keynam, down, up)
         finally:
             self.running = False
         if self.verbose:
@@ -195,17 +208,17 @@ if __name__ == "__main__":
 
     lmkb = linuxmousekeybinds("Logitech G500s Laser Gaming Mouse")
     #--
-    lmkb.bind_key_to_button("Rise of the Tomb Raider", "BTN_EXTRA",   "btn-down", "3")  # thumb button forward
-    lmkb.bind_key_to_button("Rise of the Tomb Raider", "BTN_FORWARD", "btn-down", "c")  # thumb button middle
-    lmkb.bind_key_to_button("Rise of the Tomb Raider", "BTN_SIDE",    "btn-down", "Escape")  # thumb button backward
-    lmkb.bind_key_to_button("Rise of the Tomb Raider", "REL_HWHEEL",  "whl-left", "r")  # wheel sideways left
-    lmkb.bind_key_to_button("Rise of the Tomb Raider", "REL_HWHEEL",  "whl-right", "v")  # wheel sideways right
+    lmkb.bind_key_to_button("Rise of the Tomb Raider", "BTN_EXTRA",   "k")  # thumb button forward
+    lmkb.bind_key_to_button("Rise of the Tomb Raider", "BTN_FORWARD", "c")  # thumb button middle
+    lmkb.bind_key_to_button("Rise of the Tomb Raider", "BTN_SIDE",    "Escape")  # thumb button backward
+    lmkb.bind_key_to_button("Rise of the Tomb Raider", "REL_HWHEEL+", "r")  # wheel sideways left
+    lmkb.bind_key_to_button("Rise of the Tomb Raider", "REL_HWHEEL-", "v")  # wheel sideways right
     #--
-    lmkb.bind_key_to_button("EthanCarter (64-bit, PCD3D_SM5)", "BTN_SIDE", "btn-down", "Escape")  # thumb button backward
+    lmkb.bind_key_to_button("EthanCarter (64-bit, PCD3D_SM5)", "BTN_SIDE", "Escape")  # thumb button backward
     #--
-    lmkb.bind_key_to_button("Layers of Fear", "BTN_SIDE", "btn-down", "Escape")  # thumb button backward
+    lmkb.bind_key_to_button("Layers of Fear", "BTN_SIDE", "Escape")  # thumb button backward
     #--
-#    lmkb.bind_key_to_button(None, "BTN_SIDE", "btn-down", "4,3") # thumb button backward
+    lmkb.bind_key_to_button(None, "BTN_SIDE", "1") # default binding for any other window
     #--
     lmkb.run()
     #--
@@ -215,16 +228,19 @@ if __name__ == "__main__":
 #    #-- EXAMPLES --
 #    lmkb = linuxmousekeybinds("Logitech USB Optical Mouse") # RX 1000
 #    #--
-#    lmkb.bind_key_to_button(None, "BTN_EXTRA",   "btn-down",  "1") # thumb button forward
-#    lmkb.bind_key_to_button(None, "BTN_FORWARD", "btn-down",  "2") # thumb button middle
-#    lmkb.bind_key_to_button(None, "BTN_SIDE",    "btn-down",  "3") # thumb button backward
-#    lmkb.bind_key_to_button(None, "REL_HWHEEL",  "whl-left",  "4") # wheel sideways left
-#    lmkb.bind_key_to_button(None, "REL_HWHEEL",  "whl-right", "5") # wheel sideways right
-#    lmkb.bind_key_to_button(None, "REL_WHEEL",   "whl-up",    "6") # wheel up
-#    lmkb.bind_key_to_button(None, "REL_WHEEL",   "whl-down",  "7") # wheel down
+#    lmkb.bind_key_to_button(None, "BTN_EXTRA",   "1") # thumb button forward
+#    lmkb.bind_key_to_button(None, "BTN_FORWARD", "2") # thumb button middle
+#    lmkb.bind_key_to_button(None, "BTN_SIDE",    "3") # thumb button backward
+#    lmkb.bind_key_to_button(None, "REL_HWHEEL+", "4") # wheel sideways left
+#    lmkb.bind_key_to_button(None, "REL_HWHEEL-", "5") # wheel sideways right
+#    lmkb.bind_key_to_button(None, "REL_WHEEL+",  "6") # wheel up
+#    lmkb.bind_key_to_button(None, "REL_WHEEL-",  "7") # wheel down
 #    #--
-#    lmkb.bind_key_to_button(7154, "BTN_SIDE", "btn-down", "3") # binding by PID instead of window-name
+#    lmkb.bind_key_to_button(7154, "BTN_SIDE", "3") # binding by PID instead of window-name
 #    #--
-#    lmkb.bind_key_to_button("Rise of the Tomb Raider™", "BTN_EXTRA",   "btn-down", "1") # thumb button forward
-#    lmkb.bind_key_to_button("Rise of the Tomb Raider™", "BTN_FORWARD", "btn-down", "2") # thumb button middle
-#    lmkb.bind_key_to_button("Rise of the Tomb Raider™", "BTN_SIDE",    "btn-down", "Escape") # thumb button backward
+#    lmkb.bind_key_to_button("Rise of the Tomb Raider™", "BTN_EXTRA",   "1") # thumb button forward
+#    lmkb.bind_key_to_button("Rise of the Tomb Raider™", "BTN_FORWARD", "2") # thumb button middle
+#    lmkb.bind_key_to_button("Rise of the Tomb Raider™", "BTN_SIDE",    "Escape") # thumb button backward
+#    #--
+#    print lmkb.btns, "\n"
+#    print lmkb.cfgs, "\n"
